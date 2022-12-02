27e3f73ae0468f1b2512ee88c7d1e6db1f9bf3ee
dieblommetjie <tarrynvdvyver@outlook.com>
2022-12-02 07:25:57 +0200

    updated variable names to be readable

--- day2_solution.py
+++ day2_solution.py
@@ -1,18 +1,18 @@
-X = [l.strip() for l in open('day2_input.txt')]
-p1 = 0
-p2 = 0
-for x in X:
-    op,me = x.split()
-    p1 += {'X': 1, 'Y': 2, 'Z': 3}[me]
-    p1 += {('A', 'X'): 3, ('A', 'Y'): 6, ('A', 'Z'): 0,
+input = [l.strip() for l in open('day2_input.txt')]
+strategy_one = 0
+strategy_two = 0
+for x in input:
+    opponent,me = x.split()
+    strategy_one += {'X': 1, 'Y': 2, 'Z': 3}[me]
+    strategy_one += {('A', 'X'): 3, ('A', 'Y'): 6, ('A', 'Z'): 0,
             ('B', 'X'): 0, ('B', 'Y'): 3, ('B', 'Z'): 6,
             ('C', 'X'): 6, ('C', 'Y'): 0, ('C', 'Z'): 3,
-            }[(op, me)]
+            }[(opponent, me)]
 
-    p2 += {'X': 0, 'Y': 3, 'Z': 6}[me]
-    p2 += {('A', 'X'): 3, ('A', 'Y'): 1, ('A', 'Z'): 2,
+    strategy_two += {'X': 0, 'Y': 3, 'Z': 6}[me]
+    strategy_two += {('A', 'X'): 3, ('A', 'Y'): 1, ('A', 'Z'): 2,
             ('B', 'X'): 1, ('B', 'Y'): 2, ('B', 'Z'): 3,
             ('C', 'X'): 2, ('C', 'Y'): 3, ('C', 'Z'): 1,
-            }[(op, me)]
-print(p1)
-print(p2)
+            }[(opponent, me)]
+print(strategy_one)
+print(strategy_two)
