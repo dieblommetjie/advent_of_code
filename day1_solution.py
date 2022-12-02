cfcfa091c30010aa70b2fdf726ef680685aa330d
dieblommetjie <tarrynvdvyver@outlook.com>
2022-12-02 07:23:21 +0200

    day 2

--- day1_solution.py
+++ day1_solution.py
@@ -3,5 +3,7 @@ elf_totals = []
 
 for each_elf in elf_blocks:
     elf_totals.append(sum(int(x) for x in each_elf.split("\n")))
+elf_totals = sorted(elf_totals, reverse=True)
 
 print(max(elf_totals))
+print(elf_totals[0]+elf_totals[1]+elf_totals[2])
